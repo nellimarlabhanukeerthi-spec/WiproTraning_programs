87965746ed722dbd1b4a811f09d8b3971f7c8e3c
NellimarlaBhanuKeerthi <nellimarlabhanukeerthi@gmail.com>
2025-11-29 19:57:52 +0530

    Add PrintNumbers.java to print numbers 1 to 10
    Implemented a Java program to print numbers from 1 to 10 using a for loop.

---
+++ Day2/PrintNumbers.java
@@ -0,0 +1,9 @@
+/* Write a Java program to print numbers from 1 to 10 using a for loop.*/
+
+public class PrintNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
