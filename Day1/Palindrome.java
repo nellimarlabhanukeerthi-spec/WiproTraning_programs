8d41485d09408d7c14a880dacd3cf32edfdbce71
NellimarlaBhanuKeerthi <nellimarlabhanukeerthi@gmail.com>
2025-11-29 19:48:55 +0530

    Create  Palindrome.java

---
+++ Day1/Palindrome.java
@@ -0,0 +1,22 @@
+/*Take User Input, and check if a word is a Palindrome.*/
+
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
+
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+
+        sc.close();
+    }
+}
